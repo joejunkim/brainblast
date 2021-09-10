89ddaa6a01c2bc257c1d1c4a2e273c178fcd3e5b
Joe Kim <joekim6a@gmail.com>
2021-09-09 22:14:57 -0700

    styling navbar

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,11 +1,13 @@
 
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import LogoutButton from './auth/LogoutButton';
+import LogoutButton from '../auth/LogoutButton';
+
+import './NavBar.css'
 
 const NavBar = () => {
   return (
-    <nav>
+    <nav id='navbar__container'>
       <ul>
         <li>
           <NavLink to='/' exact={true} activeClassName='active'>
